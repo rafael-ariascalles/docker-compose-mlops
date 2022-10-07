a970487df4eaaa849eb9400877955fed41c96743
rafael-ariascalles <rafael-ariascalles@github.com>
2022-10-06 20:00:01 -0600

    <code> docker compose configuration

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from starlette.responses import StreamingResponse
 from fastapi import FastAPI, File, UploadFile
 import requests
-
+import os
 #We generate a new FastAPI app in the Prod environment
 #https://fastapi.tiangolo.com/
 
+app = FastAPI()
 
+@app.get("/")
 #Call your get function for a health Check
-#to receive both (face-bokeh and face-emotion)
+async def healthcheck():
+    #to receive both (face-bokeh and face-emotion)
+    service_boke = requests.get(os.getenv('SERVICE_BOKE'))
+    service_emotion = requests.get(os.getenv('SERVICE_EMOTION'))
+
+    return "OK"
